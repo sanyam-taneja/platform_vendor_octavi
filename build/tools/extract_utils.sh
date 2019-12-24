9993b276900fb86bf6f2b6c14b5380ed1c70b76e
XiNGRZ <chenxingyu92@gmail.com>
2019-12-24 10:37:13 +0800

    extract_utils: Fix bad substitution for zsh
    Change-Id: I5f4fabc62b1ac3da159b36c3ae6080e145948530

--- build/tools/extract_utils.sh
+++ build/tools/extract_utils.sh
@@ -1174,7 +1174,7 @@ function oat2dex() {
     local SRC="$3"
     local TARGET=
     local OAT=
-    local HOST="$(uname)"
+    local HOST="$(uname | tr '[:upper:]' '[:lower:]')"
 
     if [ -z "$BAKSMALIJAR" ] || [ -z "$SMALIJAR" ]; then
         export BAKSMALIJAR="$OCTAVI_ROOT"/prebuilts/tools/smali/baksmali.jar
@@ -1182,11 +1182,11 @@ function oat2dex() {
     fi
 
     if [ -z "$VDEXEXTRACTOR" ]; then
-        export VDEXEXTRACTOR="$OCTAVI_ROOT"/prebuilts/tools-extras/"${HOST,,}"-x86/bin/vdexExtractor
+        export VDEXEXTRACTOR="$OCTAVI_ROOT"/prebuilts/tools-extras/{HOST}-x86/bin/vdexExtractor
     fi
 
     if [ -z "$CDEXCONVERTER" ]; then
-        export CDEXCONVERTER="$OCTAVI_ROOT"/prebuilts/tools-extras/"${HOST,,}"-x86/bin/compact_dex_converter
+        export CDEXCONVERTER="$OCTAVI_ROOT"/prebuilts/tools-extras/{HOST}-x86/bin/compact_dex_converter
     fi
 
     # Extract existing boot.oats to the temp folder
