9c179895bf147815de6d058f00ea803bf5209eb3
Han Wang <416810799@qq.com>
2020-03-10 09:40:47 +0200

    extract_utils: Fix up 4a2b65fb60af23b6beccd8c405fc7cea90b9474d
    Change-Id: I9e5ea799812ca932de95ec6cbdedb604cbde5736

--- build/tools/extract_utils.sh
+++ build/tools/extract_utils.sh
@@ -1198,11 +1198,11 @@ function oat2dex() {
     fi
 
     if [ -z "$VDEXEXTRACTOR" ]; then
-        export VDEXEXTRACTOR="$OCTAVI_ROOT"/prebuilts/tools-extras/{HOST}-x86/bin/vdexExtractor
+        export VDEXEXTRACTOR="$OCTAVI_ROOT"/prebuilts/tools-extras/${HOST}-x86/bin/vdexExtractor
     fi
 
     if [ -z "$CDEXCONVERTER" ]; then
-        export CDEXCONVERTER="$OCTAVI_ROOT"/prebuilts/tools-extras/{HOST}-x86/bin/compact_dex_converter
+        export CDEXCONVERTER="$OCTAVI_ROOT"/prebuilts/tools-extras/${HOST}-x86/bin/compact_dex_converter
     fi
 
     # Extract existing boot.oats to the temp folder
