44a698da40b7216164f2cdc50173f5a9cd3da7b2
luca020400 <luca020400@users.noreply.github.com>
2020-07-01 20:42:26 +0000

    kernel: Allow specifying a custom linker binary
    * This is needed for using LLVM LLD ( and ThinLTO )
    
    Change-Id: If603274326f0ff22f967455847ffe1c595f04aa5

--- config/bootanimation.mk
+++ config/bootanimation.mk
@@ -18,11 +18,11 @@
 TARGET_BOOT_ANIMATION_RES ?= undefined
 
 ifeq ($(TARGET_BOOT_ANIMATION_RES),720)
-     PRODUCT_COPY_FILES += vendor/colt/prebuilt/common/media/720.zip:system/media/bootanimation.zip
+     PRODUCT_COPY_FILES += vendor/octavi/bootanimation/720.zip:system/media/bootanimation.zip
 else ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
-     PRODUCT_COPY_FILES += vendor/colt/prebuilt/common/media/1080.zip:system/media/bootanimation.zip
+     PRODUCT_COPY_FILES += vendor/octavi/bootanimation/1080.zip:system/media/bootanimation.zip
 else ifeq ($(TARGET_BOOT_ANIMATION_RES),1440)
-     PRODUCT_COPY_FILES += vendor/colt/prebuilt/common/media/1440.zip:system/media/bootanimation.zip
+     PRODUCT_COPY_FILES += vendor/octavi/bootanimation/1440.zip:system/media/bootanimation.zip
 else ifeq ($(TARGET_BOOT_ANIMATION_RES),undefined)
      $(warning Target bootanimation res is undefined, using generic bootanimation )
      PRODUCT_COPY_FILES += vendor/octavi/bootanimation/bootanimation.zip:system/media/bootanimation.zip
