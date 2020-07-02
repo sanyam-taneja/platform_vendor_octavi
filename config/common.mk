theOverpoweredGuy/android_vendor_zenx

config: Split priv-app permission whitelist

config/common.mk
@@ -82,7 +82,8 @@ PRODUCT_COPY_FILES += \
 
 # This is ZenX!
 PRODUCT_COPY_FILES += \
-    vendor/zenx/config/permissions/privapp-permissions-zenx.xml:system/etc/permissions/privapp-permissions-zenx.xml
+    vendor/zenx/config/permissions/privapp-permissions-zenx-system.xml:system/etc/permissions/privapp-permissions-zenx.xml \
+    vendor/zenx/config/permissions/privapp-permissions-zenx-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-zenx.xml \
 
 # Hidden API whitelist
 PRODUCT_COPY_FILES += \

sanyam-taneja/platform_vendor_octavi

config/common.mk
@@ -82,7 +82,8 @@ PRODUCT_COPY_FILES += \
 
 # This is Octavi!
 PRODUCT_COPY_FILES += \
-    vendor/octavi/config/permissions/privapp-permissions-octavi.xml:system/etc/permissions/privapp-permissions-octavi.xml
+    vendor/octavi/config/permissions/privapp-permissions-octavi.xml:system/etc/permissions/privapp-permissions-octavi.xml \
+    vendor/octavi/config/permissions/privapp-permissions-octavi-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-octavi.xml \
 
 # Hidden API whitelist
 PRODUCT_COPY_FILES += \