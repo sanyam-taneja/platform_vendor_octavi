LineageOS/android_vendor_lineage

backuptool: Remove backup blacklist/whitelist
Change-Id: Ia89e6fa4c31377a429e0dfa912d0b61393696009

config/common.mk
@@ -30,8 +30,7 @@ endif
 PRODUCT_COPY_FILES += \
     vendor/lineage/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
     vendor/lineage/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
-    vendor/lineage/prebuilt/common/bin/50-lineage.sh:$(TARGET_COPY_OUT_SYSTEM)/addon.d/50-lineage.sh \
-    vendor/lineage/prebuilt/common/bin/blacklist:$(TARGET_COPY_OUT_SYSTEM)/addon.d/blacklist
+    vendor/lineage/prebuilt/common/bin/50-lineage.sh:$(TARGET_COPY_OUT_SYSTEM)/addon.d/50-lineage.sh
 
 ifneq ($(AB_OTA_PARTITIONS),)
 PRODUCT_COPY_FILES += \

sanyam-taneja/platform_vendor_octavi

config/common.mk
@@ -43,8 +43,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
 PRODUCT_COPY_FILES += \
     vendor/octavi/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
     vendor/octavi/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
-    vendor/octavi/prebuilt/common/bin/50-octavi.sh:$(TARGET_COPY_OUT_SYSTEM)/addon.d/50-octavi.sh \
-    vendor/octavi/prebuilt/common/bin/blacklist:$(TARGET_COPY_OUT_SYSTEM)/addon.d/blacklist
+    vendor/octavi/prebuilt/common/bin/50-octavi.sh:$(TARGET_COPY_OUT_SYSTEM)/addon.d/50-octavi.sh
 
 ifneq ($(AB_OTA_PARTITIONS),)
 PRODUCT_COPY_FILES += \